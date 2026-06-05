edd52fc567b153eb2d9548f360c90bf477331539
chae <dldmd33333@gmail.com>
2026-06-05 17:15:16 +0900

    feat: swiper 수정

--- js/main.js
+++ js/main.js
@@ -4,39 +4,42 @@ import { renderFooter } from "./module/footer.js";
 document.addEventListener("DOMContentLoaded", () => {
   renderHeader();
   renderFooter();
-  if (document.querySelector(".testimonial")) {
-    new Swiper(".testimonial", {
+  if (document.querySelector(".testimonial-swiper")) {
+    new Swiper(".testimonial-swiper", {
       loop: true,
       slidesPerView: 1,
       navigation: {
-        nextEl: ".testimonial .swiper-button-next",
-        prevEl: ".testimonial .swiper-button-prev",
+        nextEl: ".testimonial-swiper .swiper-button-next",
+        prevEl: ".testimonial-swiper .swiper-button-prev",
       },
     });
   }
-  if (document.querySelector(".portfolio")) {
-    new Swiper(".portfolio", {
+  if (document.querySelector(".portfolio-swiper")) {
+    new Swiper(".portfolio-swiper", {
       loop: true,
       slidesPerView: 1,
       navigation: {
-        nextEl: ".portfolio .swiper-button-next",
-        prevEl: ".portfolio .swiper-button-prev",
+        nextEl: ".portfolio-swiper .swiper-button-next",
+        prevEl: ".portfolio-swiper .swiper-button-prev",
       },
       pagination: {
-        el: ".portfolio .swiper-pagination",
-        clickable: true,
+        el: ".portfolio-swiper .swiper-pagination",
+        clickable: true, 
       },
     });
   }
 });
 const header = document.querySelector("body > header");
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 0) {
-    header.classList.add("fixed");
-  } else {
-    header.classList.remove("fixed");
-  }
-});
+if (header) {
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+      header.classList.add("fixed");
+    } else {
+      header.classList.remove("fixed");
+    }
+  });
+}
+
 const questions = document.querySelectorAll(".qnalist details");
 questions.forEach((question) => {
   question.addEventListener("click", (e) => {
